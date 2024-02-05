1865a400c48213b5e4291519f53fa2b346b19248
Leonel-R <leonel.reyes1426@gmail.com>
2024-02-04 21:07:05 -0500

    Added capitalized function from previous exercise to make playerSelection case insensitive. Also added playRound function which determines winning player based on rock paper scissor rules

--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 3)
     if (randNum == 0) {
         return 'Rock'
@@ -7,3 +7,33 @@ function getComputerChoice () {
     }
     return 'Scissors'
 }
+
+function capitalize(str) {
+    let firstLetter = str.charAt(0)
+    let restOfString = str.slice(1).toLowerCase()
+     return firstLetter.toUpperCase() + restOfString
+}
+
+function playRound(playerSelection,computerSelection) {
+    let formattedPlayerSelection = capitalize(playerSelection)
+    if (formattedPlayerSelection == computerSelection) {
+        return 'Draw!'
+    } else if (formattedPlayerSelection == 'Rock' && computerSelection == 'Paper') {
+        return 'You Lose! Paper beats Rock'
+    } else if (formattedPlayerSelection == 'Paper' && computerSelection == 'Rock') {
+        return 'You Win! Paper beats Rock'
+    } else if (formattedPlayerSelection == 'Rock' && computerSelection == 'Scissors') {
+        return 'You Win! Rock beats Scissors'
+    } else if (formattedPlayerSelection == 'Scissors' && computerSelection == 'Paper') {
+        return 'You Win! Scissors beats Paper'
+    } else if (formattedPlayerSelection == 'Scissors' && computerSelection == 'Rock') {
+        return 'You Lose! Rock beats Scissors'
+    }  else if (formattedPlayerSelection == 'Paper' && computerSelection == 'Scissors') {
+        return 'You Lose! Scissors beats Paper'
+    }
+}
+
+let playerSelection = "PaPeR"
+let computerSelection = getComputerChoice()
+
+// console.log(playRound(playerSelection,computerSelection))
